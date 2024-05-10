6b0c3f0b36efb209616b7a7547926d000b60f7ec
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-10 14:41:39 -0600

    Creando el coontaddor dede 0 	deleted:    lib/consts/global_colors.dart 	modified:   lib/main.dart 	new file:   lib/presentation/screens/counter_screen.dart 	deleted:    lib/screens/home_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:sc_orientation/presentation/screens/counter_screen.dart';
 
-import 'consts/global_colors.dart';
-import 'screens/home_screen.dart';
-
-void main() {
+void main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget{
+  //Inicializa el contructor de Myapp
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Flutter 3.0.4',
-    theme: ThemeData(
-      scaffoldBackgroundColor: lightScaffoldColor,
-      primaryColor: lightCardColor,
-      colorScheme: ColorScheme.light(
-        background: lightBackgroundColor,
-        // Puedes configurar otros colores aquí según sea necesario
-        secondary: lightIconsColor,
-      ),
-      appBarTheme: AppBarTheme(
-        iconTheme: IconThemeData(
-          color: lightIconsColor,
-        ),
-        backgroundColor: lightScaffoldColor,
-        centerTitle: true,
-        titleTextStyle: TextStyle(
-          color: lightTextColor,
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
-        ),
-        elevation: 0,
-      ),
-      iconTheme: IconThemeData(
-        color: lightIconsColor,
-      ),
-      textSelectionTheme: const TextSelectionThemeData(
-        cursorColor: Colors.black,
-        selectionColor: Colors.blue,
-        // selectionHandleColor: Colors.blue,
-      ),
-      cardColor: lightCardColor,
-    ),
-    home: const HomeScreen(),
-  );
-}
-
+    return const MaterialApp(
+      //Eliminar Equeta de modo desarrollo
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen()
+    );
+  }
+  
 }
